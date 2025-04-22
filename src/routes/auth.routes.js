6f1015dc409ebbfb9d1b69b2bee3f415ea44336c
Stavan7 <kulkarnistavan25@gmail.com>
2025-04-22 19:20:57 +0530

    update auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,24 @@
-import express from 'express'
-import protectedRoute from '../middleware/auth.middleware.js';
-import { login, register, userprofile } from '../controllers/auth.controller.js';
+import express from "express";
+import {
+	login,
+	register,
+	userprofile,
+	resetPassword,
+	updateProfile,
+	sendResetCode,
+	verifyPasswordResetCode,
+} from "../controllers/auth.controller.js";
+import protectedRoute from "../middleware/auth.middleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", register)
-router.post("/login", login)
-router.get("/userprofile", protectedRoute, userprofile)
+router.post("/register", register);
+router.post("/login", login);
+router.get("/userprofile", protectedRoute, userprofile);
+router.patch("/updateProfile", protectedRoute, updateProfile);
+//passsword reset func
+router.post("/send-reset-code", sendResetCode);
+router.post("/verify-reset-code", verifyPasswordResetCode);
+router.post("/reset-password", resetPassword);
 
-export default router
+export default router;
