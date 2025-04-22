c8ed9cf62a59c66a9161f08aeed00d847bc943c9
Stavan7 <kulkarnistavan25@gmail.com>
2025-04-22 22:30:38 +0530

    re init cron jobs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
+import job from './lib/cron.js'
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import postRoutes from "./routes/post.routes.js";
@@ -11,6 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+job.start(); // Start the cron job
 app.use(cors());
 app.use(express.json({ limit: "10mb" })); //middlewawre to handle body
 
